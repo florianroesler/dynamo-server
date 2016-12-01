e7c3f9cba4ac9a75792ce6a89a16993e1a546faa
Florian Roesler <florian.roesler@wooga.net>
2016-12-01 13:03:22 +0100

    added pricing and fixed booking and unbooking instances

---
+++ lib/pricing.rb
@@ -0,0 +1,24 @@
+require 'net/http'
+require 'json'
+class Pricing
+  def self.prices
+    @@prices ||= begin
+      uri = URI.parse("http://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js")
+      https = Net::HTTP.new(uri.host,uri.port)
+      req = Net::HTTP::Get.new(uri.path)
+      res = https.request(req)
+
+      body = res.body.split('callback(')[1].strip[0..-3]
+      body = body.gsub(/{\s*(\w)/, '{"\1').gsub(/,\s*(\w)/, ',"\1').gsub(/(\w):/, '\1":')
+
+      prices = JSON.parse(body)
+      region = Java::FrDynamoEc2::MachineManager.getInstance.getRegion
+      region_object = prices['config']['regions'].find { |r| r['region'] == region }
+      instances = region_object['instanceTypes'].map do |types|
+        types['sizes'].map do |size|
+          { type: size['size'], price: size['valueColumns'].first['prices'].values.first }
+        end
+      end.flatten
+    end
+  end
+end
--- routes/machines.rb
+++ routes/machines.rb
@@ -5,6 +5,19 @@ get '/machines' do
 end
 
 post '/machines/book' do
-  count = params['count'] || 1
-  Java::FrDynamoEc2::MachineManager.getInstance.bookInstance(count)
+  @count = params['count'].to_i
+  @type = params['type']
+
+  Java::FrDynamoEc2::MachineManager.getInstance.bookInstance(@count, @type)
+
+  haml :machines_booked
+end
+
+post '/machines/unbook' do
+  id = params['instance_id']
+  instance = Java::FrDynamoEc2::NodeList.getInstance.getNodes.find { |i| i.getInstanceId == id }
+
+  Java::FrDynamoEc2::MachineManager.getInstance.terminateInstances(java.util.ArrayList.new([instance]))
+
+  redirect '/machines'
 end
