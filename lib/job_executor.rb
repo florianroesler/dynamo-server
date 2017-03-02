d77fe1e42d39a0111865ed73cf0b293c3be78310
Florian Roesler <florian.roesler@wooga.net>
2017-03-02 14:04:35 +0100

    fixed stuff to fit new DynamOpenCL version

--- lib/job_executor.rb
+++ lib/job_executor.rb
@@ -3,19 +3,19 @@ class JobExecutor
 
   def self.matrix
     Thread.new do
-      Java::FrDynamoSamplesMatrix_multiplication.MatrixMain.main(["5120","4"])
+      Java::FrDynamoSamplesMatrix_multiplication.MatrixMain.main(["3200","8"])
     end
   end
 
   def self.mandelbrot
     Thread.new do
-      Java::FrDynamoSamplesMandelbrot.MandelbrotMain.main(["5000","10000","10", "0"])
+      Java::FrDynamoSamplesMandelbrot.MandelbrotMain.main(["2000","100000","20", "0"])
     end
   end
 
   def self.kmeans
     Thread.new do
-      Java::FrDynamoSamplesKmeans.KMeansMain.main(["10000","25","10"])
+      Java::FrDynamoSamplesKmeans.KMeansMain.main(["100000","50"])
     end
   end
 
--- routes/devices.rb
+++ routes/devices.rb
@@ -1,5 +1,6 @@
 get '/devices' do
   @devices = Java::FrDynamoEc2::NodeList.getInstance.getAllDevices
+  @cloud_devices = Java::FrDynamoEc2::NodeList.getInstance.getCloudDevices
 
   haml :devices
 end
